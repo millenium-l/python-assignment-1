ffae3751f8bad47f295cd83aa69ed1462a0092ee
millenium-l <156191409+millenium-l@users.noreply.github.com>
2024-07-03 19:11:45 +0300

    Add files via upload

---
+++ 05. assignment one/hello.py
@@ -0,0 +1,15 @@
+# input from the user
+num1 = int(input("Enter num 1: "))
+num2 = int(input("Enter num 2: "))
+
+# perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication =  num1 * num2
+division = num1 / num2
+
+# print the outcome 
+print(f"Result of addition is: {addition}\n")
+print(f"Result of subtraction is: {subtraction}\n")
+print(f"Result of multiplication is: {multiplication}\n")
+print(f"Result of division is: {division}\n")
